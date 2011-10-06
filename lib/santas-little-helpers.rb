794cec5eb694c9e10a1a87a07df1f5469e829fea
Stephan Schubert <stephan@frozencherry.de>
2011-10-06 21:56:39 +0200

    Load the new extension by default

--- lib/santas-little-helpers.rb
+++ lib/santas-little-helpers.rb
@@ -15,4 +15,5 @@ require "santas-little-helpers/settings"
 if defined?(Rails)
   require "santas-little-helpers/rails/helpers/media_block"
   require "santas-little-helpers/rails/helpers/date_helper"
+  require "santas-little-helpers/rails/helpers/textile_helper"
 end
